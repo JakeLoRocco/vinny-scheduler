d2ff167054a7ca41e990a27d84e6400d3fc3c41d
jake lorocco <jakelorocco@gmail.com>
2020-09-03 16:35:34 -0400

    Updates

---
+++ precache-manifest.8c756c6d7bac496d8ebeb549cb74bdb9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4352825bac6ca8bc26d6353a3ac62b79",
+    "url": "/vinny-scheduler/index.html"
+  },
+  {
+    "revision": "f61a9c532e6fe232d78e",
+    "url": "/vinny-scheduler/static/css/main.bb3db789.chunk.css"
+  },
+  {
+    "revision": "8f544782791e66aaf637",
+    "url": "/vinny-scheduler/static/js/2.54df3f3c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/vinny-scheduler/static/js/2.54df3f3c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f61a9c532e6fe232d78e",
+    "url": "/vinny-scheduler/static/js/main.92050609.chunk.js"
+  },
+  {
+    "revision": "cd61ac3d949e919a9ae7",
+    "url": "/vinny-scheduler/static/js/runtime-main.5d7d5965.js"
+  }
+]);
